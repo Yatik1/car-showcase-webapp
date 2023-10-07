31795173bd3fb3f738b96aa8c305981711cc0593
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-07 13:52:29 +0530

    porps interface

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtomProps {
+    title:string;
+    containerStyles?:string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
