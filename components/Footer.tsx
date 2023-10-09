4bd3e78900614d139387dbdcd9145b75836fd293
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-09 14:03:46 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,6 +31,20 @@ const Footer = () => {
              ))}
            </div>
         </div>
+
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+      <p>@2023 CarHub. All rights reserved</p>
+
+      <div className="footer__copyrights-link">
+        <Link href="/" className="text-gray-500">
+          Privacy & Policy
+        </Link>
+        <Link href="/" className="text-gray-500">
+          Terms & Condition
+        </Link>
+      </div>
+    </div>
+    
      </footer>
   )
 }
