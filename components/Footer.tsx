400a84ef1c27799a4ff44bdb5f70764ae83a685f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-09 13:52:51 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
-       Footer
-    </div>
+     <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+        <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+           <div className="flex flex-col justify-start items-start gap-6">
+              <Image src='/logo.svg' alt='logo' width={118} height={18} 
+                   className='object-contain' />
+                   <p className='text-base text-gray-700'>
+                   Carhub @2023 <br />
+                   All rights reserved &copy;
+                   </p>
+           </div>
+        </div>
+     </footer>
   )
 }
 
