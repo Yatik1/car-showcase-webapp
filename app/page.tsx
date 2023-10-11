b790567362258c5789380bd323150e38226d7fd6
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-11 17:24:24 +0530

    ::fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  console.log(allCars)
+
   return (
      <main className="overflow-hidden">
         <Hero />
---
+++ utils/index.ts
@@ -0,0 +1,33 @@
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': ,
+// 		'X-RapidAPI-Host': '
+// 	}
+// };
+
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+const url = process.env.URI;
+
+export async function fetchCars() {
+ const headers = {
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': process.env.API_HOST,
+ }
+
+ const response = await fetch(url, {
+    headers: headers,
+ });
+
+ const result = await response.json();
+ return result;
+}
