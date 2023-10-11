718ce9e04cc6c4a31141142aea1ac602fe305943
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-11 12:07:26 +0530

    Custom search filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CustomFilter = () => {
   return (
     <div>
-      f9enhuinhm
+        Custom Filter
     </div>
   )
 }
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -21,7 +21,7 @@ const SearchManufacturer = ({manufacturer , setManufacturer} : SearchManufacture
 
     return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
             <div className="relative w-full">
                 <Combobox.Button className="absolute top-[14px]">
                      <Image 
