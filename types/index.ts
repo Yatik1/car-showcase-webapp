d5f6488d91e7aa105e8b9840733cb63d7b00023c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-11 11:34:40 +0530

    porps types

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtomProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btntype?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+    manufacturer: string,
+    setManufacturer: (manufacturer: string) => void;
+}
