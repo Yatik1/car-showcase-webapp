8573172555fb855ef63ab4246eb7410eaf5f53e1
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-11 17:35:58 +0530

    .env

--- utils/index.ts
+++ utils/index.ts
@@ -16,12 +16,12 @@
 // 	console.error(error);
 // }
 
-const url = process.env.URI;
+const url = process.env.URI!;
 
 export async function fetchCars() {
  const headers = {
-    'X-RapidAPI-Key': process.env.API_KEY,
-    'X-RapidAPI-Host': process.env.API_HOST,
+    'X-RapidAPI-Key': process.env.API_KEY!,
+    'X-RapidAPI-Host': process.env.API_HOST!,
  }
 
  const response = await fetch(url, {
