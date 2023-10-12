7c0df6d0842b941d31afd2947e1494958358f068
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-12 17:31:23 +0530

    card component

--- components/CustomButtom.tsx
+++ components/CustomButtom.tsx
@@ -4,7 +4,7 @@ import { CustomButtomProps } from "@/types";
 import Image from "next/image"
 
 const CustomButtom = ({title,containerStyles , 
-  handleClick,btntype} : CustomButtomProps) => {
+  handleClick,btntype,textStyles,rightIcon} : CustomButtomProps) => {
   return (
     <button 
       disabled={false}
@@ -12,9 +12,14 @@ const CustomButtom = ({title,containerStyles ,
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
      >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
               {title}
         </span>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+             <Image src={rightIcon} alt="right icon" fill className="object-contain" />
+          </div>
+        ) }
     </button>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,9 @@ export interface CustomButtomProps {
     containerStyles?:string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btntype?: "button" | "submit";
+    textStyles?: string; 
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
