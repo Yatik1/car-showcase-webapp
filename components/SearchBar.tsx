47f53c8a928d4ba5b9602a4d299ec4f0e219b8ae
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-17 17:07:30 +0530

    search component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,10 +2,28 @@
 
 import { useState } from "react"
 import { SearchManufacturer } from "."
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+
+ const SearchButton = ({otherClasses} : {otherClasses : string}) => {
+   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+     <Image 
+         src={'/magnifying.svg'}
+         alt={'search-icon'}
+         width={40}
+         height={40}
+         className="object-contain" />
+   </button>
+ }
 
 const SearchBar = () => {
     
     const [manufacturer , setManufacturer] = useState('')
+    const [model , setModel] = useState('')
+
+    const router = useRouter();
+    
     const handleSearch = () => {}
 
   return (
