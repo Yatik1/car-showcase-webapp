e33375773276dd32b67f405daa7ce523e548e3f9
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-17 16:42:56 +0530

    .env

--- utils/index.ts
+++ utils/index.ts
@@ -37,7 +37,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
    const url = new URL("https://cdn.imagin.studio/getimage");
    const { make, model, year } = car;
  
-   url.searchParams.append('customer', 'hrjavascript-mastery');
+   url.searchParams.append('customer', process.env.IMG_KEY!);
    url.searchParams.append('make', make);
    url.searchParams.append('modelFamily', model.split(" ")[0]);
    url.searchParams.append('zoomType', 'fullscreen');
