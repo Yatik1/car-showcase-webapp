6d49edb01f789fb89d1e64a50f486ef5bb7483e8
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-17 17:33:39 +0530

    Search component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import React , { useState } from "react"
 import { SearchManufacturer } from "."
 import Image from "next/image"
 import { useRouter } from "next/navigation"
@@ -62,8 +62,30 @@ const SearchBar = () => {
         <div className="searchbar__item">
             <SearchManufacturer 
                manufacturer={manufacturer}
-               setManufacturer={setManufacturer}/>
+               setManufacturer={setManufacturer} />
+
+               <SearchButton otherClasses="sm:hidden" />
+
         </div>
+        <div className="searchbar__item">
+           <Image 
+             src='/model-icon.png'
+             width={25}
+             height={25}
+             className="absolute w-[20px] h-[20px] ml-4"
+             alt="model icon" />
+
+            <input 
+              type="text"
+              name="model"
+              value={model}
+              onChange={(e) => setModel(e.target.value)}
+              placeholder="Tiguan..."
+              className="searchbar__input" />
+            <SearchButton otherClasses="sm:hidden" />
+        </div>
+
+        <SearchButton otherClasses="max-sm:hidden" />
     </form>
   )
 }
