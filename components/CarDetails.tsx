fa4d6429f644de82c900f7ebbd6b6d3ce26701dc
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-17 16:36:23 +0530

    ::detail component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { Fragment } from "react";
@@ -51,20 +52,20 @@ const CarDetails = ({isOpen, closeModel , car} : CarDetailsProps) => {
 
                      <div className="flex-1 flex flex-col gap-2">
                          <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
-                         <Image src='/hero.png' alt="car model" fill priority className="object-contain" />
+                         <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
                          </div>
 
-                         <div className="flex gap-3">
-                            <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                            <Image src='/hero.png' alt="car model" fill priority className="object-contain" />                               
-                            </div>
-                            <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                            <Image src='/hero.png' alt="car model" fill priority className="object-contain" />                               
-                            </div>
-                            <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                            <Image src='/hero.png' alt="car model" fill priority className="object-contain" />                               
-                            </div>
-                         </div>
+                        <div className='flex gap-3'>
+                           <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                             <Image src={generateCarImageUrl(car, "29")} alt='car model' fill priority className='object-contain' />
+                           </div>
+                          <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                            <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain mt-3' />
+                          </div>
+                          <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                            <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
+                           </div>
+                        </div>
                      </div>
 
                      <div className="flex-1 flex flex-col gap-2">
