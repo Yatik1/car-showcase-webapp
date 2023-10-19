333fe5b96fbba13e6dea60c1ac71fdd7284cf535
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-19 23:26:39 +0530

    pagination component

---
+++ components/ShowMore.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { ShowMoreProps } from "@/types"
+import { CustomButtom } from "."
+import { useRouter } from "next/navigation"
+import { updateSearchParams } from "@/utils"
+
+const ShowMore = ({pageNumber , isNext} : ShowMoreProps) => {
+    const router = useRouter();
+
+    const handleNavigation = () => {
+        const newLimit = (pageNumber + 1) * 10;
+        const newPathname = updateSearchParams('limit' , `${newLimit}`)
+
+        router.push(newPathname)
+    }
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+        {!isNext && (
+             <CustomButtom 
+                  btntype="button"
+                  title="Show More"
+                  containerStyles="bg-primary-blue rounded-full text-white"
+                  handleClick={handleNavigation} />
+        )}
+    </div>
+  )
+}
+
+export default ShowMore
