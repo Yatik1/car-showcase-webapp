ced87e437d71955fc831953d33d4c54588234ba7
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-19 17:08:57 +0530

    update

--- utils/index.ts
+++ utils/index.ts
@@ -28,7 +28,6 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
  
    const mileageRate = city_mpg * mileageFactor;
    const ageRate = (new Date().getFullYear() - year) * ageFactor;
-
    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
  
    return rentalRatePerDay.toFixed(0);
@@ -48,3 +47,13 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
  
    return `${url}`;
  }
+
+
+ export const updateSearchParams = (type: string, value: string) => {
+  
+   const searchParams = new URLSearchParams(window.location.search);
+   searchParams.set(type, value);
+   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+ 
+   return newPathname;
+ };
